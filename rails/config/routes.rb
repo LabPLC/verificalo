be52d9018fa1bd346c81e9d3f763b1ac009264c9
Manuel Rabade <manuel@rabade.net>
2014-02-11 17:29:26 -0600

    root apunta a static_pages#home

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -1,4 +1,6 @@
 AutoInteligente::Application.routes.draw do
+  root "static_pages#home"
+  get "static_pages/home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
