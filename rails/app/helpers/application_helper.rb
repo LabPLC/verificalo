a56e63f2a8b32c7e4006294ffa8ab6f31001eded
Manuel Rabade <manuel@rabade.net>
2014-02-11 17:28:17 -0600

    layout con bootstrap (tema cosmo)

--- rails/app/helpers/application_helper.rb
+++ rails/app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def is_root?()
+    if current_page?(controller: 'static_pages', action: 'home')
+      true
+    else
+      false
+    end
+  end
+  def is_active?(controller)
+    if params[:controller] == controller
+      "active"
+    else
+      ""
+    end
+  end
 end
