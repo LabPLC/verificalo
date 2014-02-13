3a08f426c70b733edce64f24d5b8d049f34d68f3
Manuel Rabade <manuel@rabade.net>
2014-02-12 18:45:21 -0600

    app/lib al autoload

--- rails/config/application.rb
+++ rails/config/application.rb
@@ -24,5 +24,8 @@ module AutoInteligente
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # autoload app/lib
+    config.autoload_paths += %W(#{config.root}/app/lib)
   end
 end
