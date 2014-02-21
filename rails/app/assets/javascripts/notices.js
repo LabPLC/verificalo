d8374911359d988a1f1277cbf80ef69f55c2ac0f
Manuel Rabade <manuel@rabade.net>
2014-02-21 13:02:43 -0600

    simplificamos forma de notices#home y notices#email, issue #29 y #20

--- rails/app/assets/javascripts/notices.js
+++ rails/app/assets/javascripts/notices.js
@@ -4,12 +4,12 @@ $(document).ready(function() {
         $('.btn-notices-home').addClass('btn-primary');
         $(this).addClass('btn-default');
         $(this).removeClass('btn-primary');
-        $.ajax({ url: btn_forms[this.id],
+        $.ajax({ url: btn_subforms[this.id],
                  success: function (data) {
-                     $('#notices-form').fadeOut('slow', function() {
-                         $('#notices-form').html(data);
-                         $("#notices-form").fadeIn('slow', function () {
-                             var y = $('#notices-form').offset().top - $('html').offset().top + $('html').scrollTop();
+                     $('#notices-subform').fadeOut('slow', function() {
+                         $('#notices-subform').html(data);
+                         $("#notices-subform").fadeIn('slow', function () {
+                             var y = $('#notices-subform').offset().top - $('html').offset().top + $('html').scrollTop();
                              $('html').animate({ scrollTop: y }, 'slow');
                          });
                      });
