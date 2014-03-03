58fe514df19cd8d231e570ecb4abcafd6d03a324
Manuel Rabade <manuel@rabade.net>
2014-03-02 22:56:09 -0600

    mailer notifier, close #39

---
+++ rails/app/mailers/notifier.rb
@@ -0,0 +1,16 @@
+class Notifier < ActionMailer::Base
+  include VehicleCDMX
+  
+  default from: "Auto Inteligente <auto_inteligente@dev.codigo.labplc.mx>"
+
+  def confirm(user)
+    @user = user
+    mail(to: user.destination, subject: "Active su suscripcion")
+  end
+  
+  def welcome(user)
+    @user = user
+    @vehicle = VehicleCDMX.new(@user.plate)
+    mail(to: user.destination, subject: "Bienvenido")
+  end
+end
