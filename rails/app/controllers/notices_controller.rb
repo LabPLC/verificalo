1ee4f487d08318afff7eed92ea1101b0bb9d5bb3
Manuel Rabade <manuel@rabade.net>
2014-03-02 22:46:08 -0600

    controlador notices, close #31

--- rails/app/controllers/notices_controller.rb
+++ rails/app/controllers/notices_controller.rb
@@ -17,11 +17,50 @@ class NoticesController < ApplicationController
   end
 
   def results
-    @params = params
-    @vehicle = VehicleCDMX.new(params[:plate]);
-    if @vehicle.error
-      redirect_to({ action: 'home' }, 
-                  { alert: { error: @vehicle.error, plate: params[:plate] }})
+    @user = User.new(user_params)
+    if !@user.save
+      @error = 'USER_PARAMS_ERROR'
+      return
+    end
+    begin      
+      @settings = settings_params.collect{ |x|
+        setting = Setting.new(x)
+        raise if setting.invalid?
+        setting
+      }
+    rescue ActionController::ParameterMissing
+      @error = 'SETTINGS_COUNT_ERROR'
+      @user.destroy
+    rescue
+      @error = 'SETTINGS_PARAMS_ERROR'
+      @user.destroy
+    else
+      @settings.each{ |s| s.save }
+      Notifier.confirm(@user).deliver
     end
   end
+  
+  def confirm
+    @debug = params
+    begin
+      @user = User.find(params[:user])
+    rescue
+      @error = 'USER_NOT_FOUND'
+    else
+      @user.confirmed_at = Time.now
+      @user.save
+      Notifier.welcome(@user).deliver
+    end
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:plate, :via, :destination)
+  end
+  
+  def settings_params
+    params.require(:settings).collect{ |k, v| { user_id: @user.id, setting: k } }
+  end
+
 end
