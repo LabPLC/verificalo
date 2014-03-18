912394c7d7a216c71d7289968483e40d75c94af5
Manuel Rabade <manuel@rabade.net>
2014-03-18 13:24:03 -0600

    simplificando el esquema de avisos, close #59

--- rails/app/models/setting.rb
+++ rails/app/models/setting.rb
@@ -2,6 +2,5 @@ class Setting < ActiveRecord::Base
   belongs_to :user
   validates :user_id, presence: true
   validates :setting, presence: true
-  validates_inclusion_of :setting, in: %w(VERIFICACION_BEFORE VERIFICACION_DURING VERIFICACION_AFTER
-                                          ADEUDOS_NEW ADEUDOS_AFTER NO_CIRCULA_WEEKDAY NO_CIRCULA_WEEKEND)
+  validates_inclusion_of :setting, in: %w(VERIFICACION ADEUDOS NO_CIRCULA_WEEKDAY NO_CIRCULA_WEEKEND)
 end
