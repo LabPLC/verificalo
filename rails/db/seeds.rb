c26b624a46288b1e7b7a8fdb36c2539969a98305
Manuel Rabade <manuel@rabade.net>
2014-04-03 15:14:19 -0600

    datos delegaciones, close #79

--- rails/db/seeds.rb
+++ rails/db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# -*- coding: utf-8 -*-
+
+Delegacion.create([{ :url => 'alvaro-obregon', :name => 'Álvaro Obregón' }, 
+                   { :url => 'azcapotzalco', :name => 'Azcapotzalco' },
+                   { :url => 'benito-juarez', :name => 'Benito Juárez' },
+                   { :url => 'coyoacan', :name => 'Coyoacán' },
+                   { :url => 'cuajimalpa', :name => 'Cuajimalpa de Morelos' },
+                   { :url => 'cuauhtemoc', :name => 'Cuauhtémoc' },
+                   { :url => 'gustavo-a-madero', :name => 'Gustavo A. Madero' },
+                   { :url => 'iztacalco', :name => 'Iztacalco' },
+                   { :url => 'iztapalapa', :name => 'Iztapalapa' },
+                   { :url => 'magdalena-contreras', :name => 'Magdalena Contreras' },
+                   { :url => 'miguel-hidalgo', :name => 'Miguel Hidalgo' },
+                   { :url => 'milpa-alta', :name => 'Milpa Alta' },
+                   { :url => 'tlahuac', :name => 'Tláhuac' },
+                   { :url => 'tlalpan', :name => 'Tlalpan' },
+                   { :url => 'venustiano-carranza', :name => 'Venustiano Carranza' },
+                   { :url => 'xochimilco', :name => 'Xochimilco' }])
