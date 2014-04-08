4803faf1feb187b5b35d5ae76338599e0de4d2ee
Manuel Rabade <manuel@rabade.net>
2014-04-08 17:44:10 -0500

    controlador answers#verificentros_delegaciones y answers#verificentros_delegacion, issues #83 y #84

--- rails/app/controllers/answers_controller.rb
+++ rails/app/controllers/answers_controller.rb
@@ -8,4 +8,24 @@ class AnswersController < ApplicationController
     @delegaciones = Delegacion.find(:all, :order => "name")
   end
 
+  def verificentros_query
+  end
+
+  def verificentros_delegaciones
+    @delegaciones = Delegacion.find(:all, :order => "name")
+  end
+  
+  def verificentros_delegacion
+    url = verificentros_delegacion_params[:delegacion]
+    @delegacion = Delegacion.find_by_url(url)
+    raise unless @delegacion
+    @verificentros = @delegacion.verificentros
+  end
+  
+  private
+  
+  def verificentros_delegacion_params
+    params.permit(:delegacion)
+  end
+
 end
