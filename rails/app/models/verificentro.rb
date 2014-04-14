25ca45a589e852028756f963d2eec73ed2e2fcd8
Manuel Rabade <manuel@rabade.net>
2014-04-14 12:49:13 -0500

    integrando geocoder en el proyecto y configurando el modelo Verificentro, close #86

--- rails/app/models/verificentro.rb
+++ rails/app/models/verificentro.rb
@@ -1,3 +1,4 @@
 class Verificentro < ActiveRecord::Base
   belongs_to :delegacion
+  reverse_geocoded_by :lat, :lon
 end
---
+++ rails/config/initializers/geocoder.rb
@@ -0,0 +1,3 @@
+# https://github.com/alexreisner/geocoder
+
+Geocoder.configure(:units => :km)
