31c2f732b09f3bc0a32d39b894b9e47aa432f9cd
Manuel Rabade <manuel@rabade.net>
2014-04-15 17:22:50 -0500

    indent

--- rails/app/assets/javascripts/notices.js
+++ rails/app/assets/javascripts/notices.js
@@ -1,7 +1,7 @@
 var btn_label = {
-  'btn-notices-home-email' : 'notices-home-email',
-  'btn-notices-home-phone' : 'notices-home-phone',
-  'btn-notices-home-twitter' : 'notices-home-twitter'
+    'btn-notices-home-email' : 'notices-home-email',
+    'btn-notices-home-phone' : 'notices-home-phone',
+    'btn-notices-home-twitter' : 'notices-home-twitter'
 };
 
 $(document).ready(function() {
