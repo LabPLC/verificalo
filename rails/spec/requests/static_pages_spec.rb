82ca7614d3b541fe4e29d805cb1eabc410d46337
Manuel Rabade <manuel@rabade.net>
2014-04-25 14:09:30 -0500

    agrupando las pruebas de static_pages

--- rails/spec/requests/static_pages_spec.rb
+++ rails/spec/requests/static_pages_spec.rb
@@ -4,17 +4,27 @@ require 'spec_helper'
 describe 'Static pages' do
   subject { page }
 
-  describe 'Home page' do
+  describe 'home' do
     before { visit root_path }
+
     it { should have_title('Verifícalo') }
-    it { should have_link('Información', href: info_path) }
-    it { should have_link('Avisos', href: avisos_path) }
-    it { should have_link('Respuestas', href: respuestas_path) }
-    it { should have_content('Conozca sus obligaciones como automovilista particular de la Ciudad de México') }
-    it { should have_field('plate') }
-    it { should have_button('Consulta') }
-    it { should have_link('Suscribirse', href: avisos_path) }
-    it { should have_link('Preguntar', href: respuestas_path) }
+
+    describe 'navbar' do
+      it { should have_link('Información', href: info_path) }
+      it { should have_link('Avisos', href: avisos_path) }
+      it { should have_link('Respuestas', href: respuestas_path) }
+    end
+    
+    describe 'jumbotron' do
+      it { should have_content('Conozca sus obligaciones como automovilista particular de la Ciudad de México') }
+      it { should have_field('plate') }
+      it { should have_button('Consulta') }
+    end
+
+    describe 'grid' do
+      it { should have_link('Suscribirse', href: avisos_path) }
+      it { should have_link('Preguntar', href: respuestas_path) }
+    end
   end
 
 end
