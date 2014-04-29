26708e72a9a04edce42851cb433fd8560483217e
Manuel Rabade <manuel@rabade.net>
2014-04-29 18:53:37 -0500

    spec del modelo delegacion, close #119

---
+++ spec/models/delegacion_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe Delegacion do
+  before do
+    @delegacion = Delegacion.new(url: 'example-borough', 
+                                 name: 'Example Borough')
+  end
+
+  subject { @delegacion }
+
+  it { should have_many(:verificentros) }
+
+  it { should respond_to(:url) }
+  it { should respond_to(:name) }
+
+  it { should be_valid }
+end
