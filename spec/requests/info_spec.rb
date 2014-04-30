67123120751a38f4a2dfdcd8b963cef827c54c43
Manuel Rabade <manuel@rabade.net>
2014-04-30 18:45:32 -0500

    pedientes pruebas de resultados, issue #108

--- spec/requests/info_spec.rb
+++ spec/requests/info_spec.rb
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 require 'spec_helper'
 
-describe 'query plate info' do
+describe 'info request' do
   subject { page }
   
-  describe 'visit home' do
+  describe 'visit info' do
     before { visit info_path }
     it { should have_field('plate') }
     it { should have_button('Continuar') }
@@ -14,7 +14,6 @@ describe 'query plate info' do
         fill_in('plate', with:  '123abc')
         click_button('Continuar') 
       end
-      it { should have_content('123ABC') }
       # TODO
     end
   end
