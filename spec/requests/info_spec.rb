6e8682fe6d4d46e11497f19346e28a7886efa953
Manuel Rabade <manuel@rabade.net>
2014-04-30 18:23:56 -0500

    sync con el spec del controlador info

--- spec/requests/info_spec.rb
+++ spec/requests/info_spec.rb
@@ -1,33 +1,21 @@
 # -*- coding: utf-8 -*-
 require 'spec_helper'
 
-describe 'Info' do
+describe 'query plate info' do
   subject { page }
-
-  describe 'home' do
+  
+  describe 'visit home' do
     before { visit info_path }
-
-    describe 'default' do
-      it { should have_content('Ingrese la placa de su auto y consulte el estado de su verificacion, hoy no circula, infracciones y tenencias.') }
-      it { should have_field('plate') }
-      it { should have_button('Continuar') }
-    end
-
-    describe 'post without plate' do
-      before { find_button('Continuar').click }
-      it { should have_content('La placa que ingreso no es valida: Debe contener números y letras.') }
-      it { should have_field('plate') }
-      it { should have_button('Continuar') }    
-    end
-
-    describe 'post invalid plate' do
+    it { should have_field('plate') }
+    it { should have_button('Continuar') }
+    
+    describe 'query plate' do
       before do
-        fill_in('plate', with: 'abcdefghijklmnopqrstuvwxyz')
-        find_button('Continuar').click 
+        fill_in('plate', with:  '123abc')
+        click_button('Continuar') 
       end
-      it { should have_content('La placa que ingreso no es valida: Debe contener máximo 14 números y letras.') }
-      it { should have_field('plate') }
-      it { should have_button('Continuar') }    
+      it { should have_content('123ABC') }
+      # TODO
     end
   end
 
