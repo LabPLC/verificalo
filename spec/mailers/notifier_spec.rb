069c6650fc65ad741bce9e824f30d9d4ed6960ac
Manuel Rabade <manuel@rabade.net>
2014-04-30 15:56:41 -0500

    spec del mail notifier, close #120

---
+++ spec/mailers/notifier_spec.rb
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+require 'spec_helper'
+
+shared_examples_for Notifier do
+  it 'render sender' do
+    expect(@mail.from).to eql(['verificalo@dev.codigo.labplc.mx'])
+  end  
+
+  it 'render destination' do
+    expect(@mail.to).to eql([@user.destination])
+  end  
+end
+
+describe Notifier do
+  describe 'confirm' do
+    before do
+      @user = FactoryGirl.create(:user_email)
+      @mail = Notifier.confirm(@user)
+    end
+
+    it_behaves_like Notifier    
+
+    it 'render subject' do 
+      expect(@mail.subject).to eql('Confirme los avisos de su auto ' + @user.plate)
+    end
+        
+    it 'show plate' do
+      expect(@mail.body.encoded).to match(@user.plate)
+    end
+    
+    it 'show confirmation url' do
+      expect(@mail.body.encoded).to match(aviso_path(@user.id))
+    end
+  end
+
+  describe 'welcome' do
+    before do
+      @user = FactoryGirl.create(:user_email_default_settings)
+      @mail = Notifier.welcome(@user)
+      @mail.body.encoded.gsub(/\r?\n/, " ")
+    end
+
+    it_behaves_like Notifier
+
+    it 'render subject' do 
+      expect(@mail.subject).to eql('Los avisos de su auto ' + @user.plate + ' estan confirmados')
+    end
+    
+    it 'show plate' do
+      expect(@mail.body.encoded).to match(@user.plate)
+    end
+
+    it 'show verificacion settings' do
+      expect(@mail.body.encoded).to match('Verificación')
+    end
+    
+    it 'show adeudos settings' do
+      expect(@mail.body.encoded).to match('Pago de tenencias e infracciones')
+    end
+    
+    it 'show no circula weekend settings' do
+      expect(@mail.body.encoded).to match('Hoy no circula sabatino')
+    end
+  end
+end
