209e5e66874cce052322cf7fe83da2728a56a075
Manuel Rabade <manuel@rabade.net>
2014-05-05 13:11:09 -0500

    sync con spec del controlador notices

--- spec/requests/notices_spec.rb
+++ spec/requests/notices_spec.rb
@@ -1,59 +1,44 @@
 # -*- coding: utf-8 -*-
 require 'spec_helper'
 
-describe "Notices" do
+describe "notices sign up" do
   subject { page }
   
   describe 'home' do
     before { visit avisos_path }
-    it { should have_content('¿Por qué medio desea recibir sus avisos?') }
     it { should have_button('Correo electrónico') }
     it { should have_button('Llamada telefónica') }
     
     describe 'via email', :js => true do
       before { click_button('Correo electrónico') }
-      it { should have_content('¿Cual es la placa de su auto y su dirección de correo electrónico?') }
-      it { should have_content('¿Qué avisos desea recibir sobre su auto?') }
+      it { should have_field('user[plate]') }
+      it { should have_field('user[destination]') }
+      it { should have_field('user[destination]') }
+      it { should have_field('settings[VERIFICACION]', :checked) }
+      it { should have_field('settings[ADEUDOS]', :checked) }
+      it { should have_field('settings[NO_CIRCULA_WEEKDAY]', :unchecked) }
+      it { should have_field('settings[NO_CIRCULA_WEEKEND]', :checked) }
       it { should have_button('Continuar') }
-      
-      describe 'post without user and plate' do
-        before { click_button('Continuar') }
-        it { should have_content('La placa que ingreso no es valida.') }
-        it { should have_content('El correo electrónico que ingreso no es valido.') }
-        it { should have_button('Continuar') }        
-      end
-      
-      describe 'post without settings' do
-        before do
-          fill_in('user[plate]', with: FactoryGirl.generate(:plate))
-          fill_in('user[destination]', with: FactoryGirl.generate(:email))
-          uncheck('settings[VERIFICACION]')
-          uncheck('settings[ADEUDOS]')
-          uncheck('settings[NO_CIRCULA_WEEKDAY]')
-          uncheck('settings[NO_CIRCULA_WEEKEND]')
-          click_button('Continuar') 
-        end
-        it { should have_content('Debe seleccionar por lo menos un aviso.') }
-        it { should have_button('Continuar') }
-      end
-      
       describe 'post' do
         before do
           fill_in('user[plate]', with: FactoryGirl.generate(:plate))
           fill_in('user[destination]', with: FactoryGirl.generate(:email))
           click_button('Continuar') 
         end
-        it { should have_content('Debe confirmar su suscripción a los avisos de su auto') }
+        it { should have_css('div.alert-info', 
+                             :text => /debe confirmar su suscripción/i) }
       end
-
+      
     end
   end
-
+  
   describe 'confirm user email' do
     before do
       @user = FactoryGirl.create(:user_email)
       visit aviso_path(@user.id)
     end
-    it { should have_content('La suscripción a los avisos de su auto con placa ' + @user.plate + ' esta confirmada.') }
+    it { should have_css('div.alert-success', 
+                         :text => /la suscripción(.*)esta confirmada/i) }
   end
+
 end
