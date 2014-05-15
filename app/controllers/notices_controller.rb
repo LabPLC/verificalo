2795ccf0820f70b45d3b4e38f1d1d9ef99528ab1
Manuel Rabade <manuel@rabade.net>
2014-05-15 18:01:06 -0500

    refactorizacion metodo new del controlador notices

--- app/controllers/notices_controller.rb
+++ app/controllers/notices_controller.rb
@@ -21,61 +21,10 @@ class NoticesController < ApplicationController
   def new
     @params = params
     @errors = Hash.new
-
-    begin
-      @user = User.new(user_params)      
-      @user.save!
-      @debug = @user.errors
-    rescue
-      if @user && @user.errors
-        if @user.errors[:plate].count > 0
-          @errors[:INVALID_PLATE] = true
-        end
-        if @user.errors[:destination].count > 0
-          @errors[:INVALID_DESTINATION] = true
-        end
-        if @errors[:INVALID_DESTINATION] || @errors[:INVALID_PLATE]
-          render 'home'
-          return
-        end
-      end
-      render 'error'
-      return
-    end
     
-    begin
-      @settings = settings_params.collect{ |x|
-        setting = Setting.new(x)
-        raise if setting.invalid?
-        setting
-      }
-    rescue ActionController::ParameterMissing
-      @user.destroy
-      @errors[:INVALID_SETTINGS_COUNT] = true
-      render 'home'
-      return
-    rescue
-      @user.destroy
-      render 'error'
-      return
-    else
-      @settings.each{ |s| s.save }
-    end
-    
-    if @user.via == 'EMAIL'
-      Notifier.confirm(@user).deliver
-    elsif @user.via == 'PHONE'
-      tw_client = Twilio::REST::Client.new(ENV['VERIFICALO_TWILIO_SSID'],
-                                           ENV['VERIFICALO_TWILIO_TOKEN'])
-      tw_req = { to: '+521' + @user.destination,
-        from: '+17542108617',
-        url: url_for({ action: 'twilio_confirm', 
-                       #host: '4ba91c34.ngrok.com', port: '80',
-                       user: @user.id }),
-        method: 'GET',
-        record: 'false' }
-      tw_client.account.calls.create(tw_req)
-    end
+    return unless new_user
+    return unless new_user_settings
+    new_user_confirm
   end
 
   def confirm
@@ -162,4 +111,65 @@ class NoticesController < ApplicationController
     params[:user]
   end
   
+  def new_user
+    begin
+      @user = User.new(user_params)
+      @user.save!
+      @debug = @user.errors
+    rescue
+      if @user && @user.errors
+        if @user.errors[:plate].count > 0
+          @errors[:INVALID_PLATE] = true
+        end
+        if @user.errors[:destination].count > 0
+          @errors[:INVALID_DESTINATION] = true
+        end
+        if @errors[:INVALID_DESTINATION] || @errors[:INVALID_PLATE]
+          render 'home'
+          return false
+        end
+      end
+      render 'error'
+      return false
+    end
+    return true
+  end
+
+  def new_user_settings
+    begin
+      @settings = settings_params.collect{ |x|
+        setting = Setting.new(x)
+        raise if setting.invalid?
+        setting
+      }
+    rescue ActionController::ParameterMissing
+      @user.destroy
+      @errors[:INVALID_SETTINGS_COUNT] = true
+      render 'home'
+      return false
+    rescue
+      @user.destroy
+      render 'error'
+      return false
+    end
+    return @settings.each{ |s| s.save }
+  end
+
+  def new_user_confirm
+    if @user.via == 'EMAIL'
+      Notifier.confirm(@user).deliver
+    elsif @user.via == 'PHONE'
+      tw_client = Twilio::REST::Client.new(ENV['VERIFICALO_TWILIO_SSID'],
+                                           ENV['VERIFICALO_TWILIO_TOKEN'])
+      tw_req = { to: '+521' + @user.destination,
+        from: '+17542108617',
+        url: url_for({ action: 'twilio_confirm', 
+                       #host: '4ba91c34.ngrok.com', port: '80',
+                       user: @user.id }),
+        method: 'GET',
+        record: 'false' }
+      tw_client.account.calls.create(tw_req)
+    end
+  end
+
 end
