da43adc0ed8b306a47d909e5b2ab3b7f4d34cdb6
Manuel Rabade <manuel@rabade.net>
2014-05-21 19:19:16 -0500

    typo en el modelo phone

--- app/models/phone.rb
+++ app/models/phone.rb
@@ -2,19 +2,19 @@ class Phone < ActiveRecord::Base
   VALID_PHONE_REGEX = /\A[0-9]{10}\z/i
   validates :number, format: { with: VALID_PHONE_REGEX }
   belongs_to :user
-
+  
   validates :cellphone, inclusion: { in: [ true, false ] }
   validates :morning, inclusion: { in: [ true, false ] }
   validates :afternoon, inclusion: { in: [ true, false ] }
   validates :night, inclusion: { in: [ true, false ] }
-
+  
   validate :schedule_present?
-
+  
   belongs_to :user
   
   def schedule_present?
     if morning.blank? and afternoon.blank? and night.blank?
-      errors.add(:notices, "You must fill in at least one schedule")
+      errors.add(:schedule, "You must fill in at least one schedule")
     end
   end
 end
