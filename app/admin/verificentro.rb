093f3366006191907e52c0d0a3a8002901a5a6af
Manuel Rabade <manuel@rabade.net>
2014-05-27 13:03:34 -0500

    parametros del admin para verificentros, fix #171

--- app/admin/verificentro.rb
+++ app/admin/verificentro.rb
@@ -1,17 +1,5 @@
 ActiveAdmin.register Verificentro do
-
-  
-  # See permitted parameters documentation:
-  # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # permit_params :list, :of, :attributes, :on, :model
-  #
-  # or
-  #
-  # permit_params do
-  #  permitted = [:permitted, :attributes]
-  #  permitted << :other if resource.something?
-  #  permitted
-  # end
-  
+  menu(priority: 1)
+  permit_params(:number, :name, :address, :delegacion_id, :phone,
+                :lat, :lon)
 end
