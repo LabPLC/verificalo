9d452cbecf9d4c596ce8e75f47f629472588b925
Manuel Rabade <manuel@rabade.net>
2014-05-27 12:55:10 -0500

    movemos la creacion del usuario administrador a seeds.rb, close #174

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,26 +3,30 @@
 require 'csv'
 require 'i18n'
 
-Delegacion.create([{ :url => 'alvaro-obregon', :name => 'Álvaro Obregón' }, 
-                   { :url => 'azcapotzalco', :name => 'Azcapotzalco' },
-                   { :url => 'benito-juarez', :name => 'Benito Juárez' },
-                   { :url => 'coyoacan', :name => 'Coyoacán' },
-                   { :url => 'cuajimalpa', :name => 'Cuajimalpa de Morelos' },
-                   { :url => 'cuauhtemoc', :name => 'Cuauhtémoc' },
-                   { :url => 'gustavo-a-madero', :name => 'Gustavo A. Madero' },
-                   { :url => 'iztacalco', :name => 'Iztacalco' },
-                   { :url => 'iztapalapa', :name => 'Iztapalapa' },
-                   { :url => 'magdalena-contreras', :name => 'Magdalena Contreras' },
-                   { :url => 'miguel-hidalgo', :name => 'Miguel Hidalgo' },
-                   { :url => 'milpa-alta', :name => 'Milpa Alta' },
-                   { :url => 'tlahuac', :name => 'Tláhuac' },
-                   { :url => 'tlalpan', :name => 'Tlalpan' },
-                   { :url => 'venustiano-carranza', :name => 'Venustiano Carranza' },
-                   { :url => 'xochimilco', :name => 'Xochimilco' }])
+AdminUser.create(email: 'admin@verificalo.mx', 
+                 password: 'verificalo',
+                 password_confirmation: 'verificalo')
+
+Delegacion.create([{ url: 'alvaro-obregon', name: 'Álvaro Obregón' },
+                   { url: 'azcapotzalco', name: 'Azcapotzalco' },
+                   { url: 'benito-juarez', name: 'Benito Juárez' },
+                   { url: 'coyoacan', name: 'Coyoacán' },
+                   { url: 'cuajimalpa', name: 'Cuajimalpa de Morelos' },
+                   { url: 'cuauhtemoc', name: 'Cuauhtémoc' },
+                   { url: 'gustavo-a-madero', name: 'Gustavo A. Madero' },
+                   { url: 'iztacalco', name: 'Iztacalco' },
+                   { url: 'iztapalapa', name: 'Iztapalapa' },
+                   { url: 'magdalena-contreras', name: 'Magdalena Contreras' },
+                   { url: 'miguel-hidalgo', name: 'Miguel Hidalgo' },
+                   { url: 'milpa-alta', name: 'Milpa Alta' },
+                   { url: 'tlahuac', name: 'Tláhuac' },
+                   { url: 'tlalpan', name: 'Tlalpan' },
+                   { url: 'venustiano-carranza', name: 'Venustiano Carranza' },
+                   { url: 'xochimilco', name: 'Xochimilco' }])
 
 CSV.foreach('db/data/verificentros.csv', :headers => true) do |r|
   next unless r['DIRECCION']
-  d = Delegacion.find_by(:url => r['DELEGACION'].downcase.gsub(/\s+/, '-'))
+  d = Delegacion.find_by(url: r['DELEGACION'].downcase.gsub(/\s+/, '-'))
   unless d
     d = Delegacion.where("unaccent(lower(name)) = ?", r['DELEGACION'].downcase).first
   end
@@ -30,11 +34,11 @@ CSV.foreach('db/data/verificentros.csv', :headers => true) do |r|
     puts '! delegacion not found: ' + r['DELEGACION']
     next
   end
-  Verificentro.create(:number => r['VERIFICENTRO'],
-                      :name => r['RAZON SOCIAL'], 
-                      :address => r['DIRECCION'], 
-                      :delegacion_id => d.id, 
-                      :phone => r['TELEFONOS'],
-                      :lat => r['LATITUD'], 
-                      :lon => r['LONGITUD']);
+  Verificentro.create(number: r['VERIFICENTRO'], 
+                      name: r['RAZON SOCIAL'], 
+                      address: r['DIRECCION'], 
+                      delegacion_id: d.id, 
+                      phone: r['TELEFONOS'],
+                      lat: r['LATITUD'],
+                      lon: r['LONGITUD']);
 end
