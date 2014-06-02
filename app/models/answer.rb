d6746b78b0c5393e13dc399e10fde821e6814402
Manuel Rabade <manuel@rabade.net>
2014-06-02 17:27:12 -0500

    eliminamos modelo related answer, close #180

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,6 +1,4 @@
 class Answer < ActiveRecord::Base
   belongs_to :category
   belongs_to :contact
-  has_many :related_answer
-  has_many :answers, :through => :related_answer
 end
--- app/models/related_answer.rb
+++
@@ -1,4 +0,0 @@
-class RelatedAnswer < ActiveRecord::Base
-  belongs_to :answer
-  belongs_to :related, :class_name => "Answer"
-end
