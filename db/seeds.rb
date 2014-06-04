88f43986a76dc2ca92cf22527194b4fe7efde549
Manuel Rabade <manuel@rabade.net>
2014-06-04 16:21:11 -0500

    preguntas y respuestas de ejemplo

--- db/seeds.rb
+++ db/seeds.rb
@@ -43,62 +43,55 @@ CSV.foreach('db/data/verificentros.csv', :headers => true) do |r|
                       lon: r['LONGITUD'])
 end
 
-Category.create([{ url: 'verificacion', name: 'Verificación', order: 1 },
-                 { url: 'hoy-no-circula', name: 'Hoy no circula', order: 2 },
-                 { url: 'infracciones', name: 'Infracciones', order: 3 },
-                 { url: 'tenencia', name: 'Tenencia', order: 4 }])
+Category.create([{ url: 'verificacion', name: 'Verificación', priority: 1 },
+                 { url: 'hoy-no-circula', name: 'Hoy no circula', priority: 2 },
+                 { url: 'infracciones', name: 'Infracciones', priority: 3 },
+                 { url: 'tenencia', name: 'Tenencia', priority: 4 }])
 
 Contact.create([{ name: 'Secretaría del Medio Ambiente', 
-                  phone: '1234 5678', phone_schedule: '10 a 5', 
+                  phone: '1234 5678', phone_schedule: '10 am a 5 pm', 
                   email: 'contacto@sedema.df.gob.mx',
                   address: 'Av. Tlaxcoaque No. 8, Col. Centro Histórico, Distrito Federal',
-                  address_schedule: '9 a 6' },
+                  address_schedule: '9 am a 6 pm', lat: '19.423219', lon: '-99.134159' },
                 { name: 'Secretaría de Finanzas',
-                  phone: '8765 4321', phone_schedule: '11 a 4', 
+                  phone: '8765 4321', phone_schedule: '11 am a 4 pm', 
                   email: 'contacto@sefin.df.gob.mx',
                   address: 'Dr. Lavista No. 144, Col. Doctores, Distrito Federal',
-                  address_schedule: '10 a 3' }])
+                  address_schedule: 'de 10 am hasta 3 pm' }])
 
-Answer.create([{ category_id: 1, contact_id: 1, url: 'verificacion-a',
-                 title: 'Pregunta A verificación', body: 'Respuesta A verificación' },
-               { category_id: 1, contact_id: 1, url: 'verificacion-b',
-                 title: 'Pregunta B verificación', body: 'Respuesta B verificación' },
-               { category_id: 1, contact_id: 1, url: 'verificacion-c',
-                 title: 'Pregunta C verificación', body: 'Respuesta C verificación' },
-               { category_id: 1, contact_id: 1, url: 'verificacion-d',
-                 title: 'Pregunta D verificación', body: 'Respuesta D verificación' },
-               { category_id: 1, contact_id: 1, url: 'verificacion-e',
-                 title: 'Pregunta E verificación', body: 'Respuesta E verificación' }])
-
-Answer.create([{ category_id: 2, contact_id: 1, url: 'hoy-no-circula-a',
-                 title: 'Pregunta A hoy no circula', body: 'Respuesta A hoy no circula' },
-               { category_id: 2, contact_id: 1, url: 'hoy-no-circula-b',
-                 title: 'Pregunta B hoy no circula', body: 'Respuesta B hoy no circula' },
-               { category_id: 2, contact_id: 1, url: 'hoy-no-circula-c',
-                 title: 'Pregunta C hoy no circula', body: 'Respuesta C hoy no circula' },
-               { category_id: 2, contact_id: 1, url: 'hoy-no-circula-d',
-                 title: 'Pregunta D hoy no circula', body: 'Respuesta D hoy no circula' },
-               { category_id: 2, contact_id: 1, url: 'hoy-no-circula-e',
-                 title: 'Pregunta E hoy no circula', body: 'Respuesta E hoy no circula' }])
+Answer.create([{ category_id: 2, url: 'hoy-no-circula-a',
+                 title: 'Pregunta A hoy no circula', body: '<p>Respuesta A hoy no circula</p>' },
+               { category_id: 2, url: 'hoy-no-circula-b',
+                 title: 'Pregunta B hoy no circula', body: '<p>Respuesta B hoy no circula</p>' },
+               { category_id: 2, url: 'hoy-no-circula-c',
+                 title: 'Pregunta C hoy no circula', body: '<p>Respuesta C hoy no circula</p>' }])
 
 Answer.create([{ category_id: 3, contact_id: 2, url: 'infracciones-a',
-                 title: 'Pregunta A infracciones', body: 'Respuesta A infracciones' },
+                 title: 'Pregunta A infracciones', body: '<p>Respuesta A infracciones</p>' },
                { category_id: 3, contact_id: 2, url: 'infracciones-b',
-                 title: 'Pregunta B infracciones', body: 'Respuesta B infracciones' },
+                 title: 'Pregunta B infracciones', body: '<p>Respuesta B infracciones</p>' },
                { category_id: 3, contact_id: 2, url: 'infracciones-c',
-                 title: 'Pregunta C infracciones', body: 'Respuesta C infracciones' },
-               { category_id: 3, contact_id: 2, url: 'infracciones-d',
-                 title: 'Pregunta D infracciones', body: 'Respuesta D infracciones' },
-               { category_id: 3, contact_id: 2, url: 'infracciones-e',
-                 title: 'Pregunta E infracciones', body: 'Respuesta E infracciones' }])
+                 title: 'Pregunta C infracciones', body: '<p>Respuesta C infracciones</p>' }])
 
 Answer.create([{ category_id: 4, contact_id: 2, url: 'tenencia-a',
-                 title: 'Pregunta A tenencia', body: 'Respuesta A tenencia' },
+                 title: 'Pregunta A tenencia', body: '<p>Respuesta A tenencia</p>' },
                { category_id: 4, contact_id: 2, url: 'tenencia-b',
-                 title: 'Pregunta B tenencia', body: 'Respuesta B tenencia' },
+                 title: 'Pregunta B tenencia', body: '<p>Respuesta B tenencia</p>' },
                { category_id: 4, contact_id: 2, url: 'tenencia-c',
-                 title: 'Pregunta C tenencia', body: 'Respuesta C tenencia' },
-               { category_id: 4, contact_id: 2, url: 'tenencia-d',
-                 title: 'Pregunta D tenencia', body: 'Respuesta D tenencia' },
-               { category_id: 4, contact_id: 2, url: 'tenencia-e',
-                 title: 'Pregunta E tenencia', body: 'Respuesta E tenencia' }])
+                 title: 'Pregunta C tenencia', body: '<p>Respuesta C tenencia</p>' }])
+
+Answer.create([{ category_id: 1, contact_id: 1, url: 'verificacion-a',
+                 title: 'Pregunta A verificación', body: '<p>Respuesta A verificación</p>',
+                 related_1_id: 1, related_2_id: 2, related_3_id: 3 },
+               { category_id: 1, contact_id: 1, url: 'verificacion-b',
+                 title: 'Pregunta B verificación', body: '<p>Respuesta B verificación</p>',
+                 related_1_id: 1, related_2_id: 2, related_3_id: 3 },
+               { category_id: 1, contact_id: 1, url: 'verificacion-c',
+                 title: 'Pregunta C verificación', body: '<p>Respuesta C verificación</p>',
+                 related_1_id: 1, related_2_id: 2, related_3_id: 3 },
+               { category_id: 1, contact_id: 1, url: 'verificacion-d',
+                 title: 'Pregunta D verificación', body: '<p>Respuesta D verificación</p>',
+                 related_1_id: 1, related_2_id: 2, related_3_id: 3 },
+               { category_id: 1, contact_id: 1, url: 'verificacion-e',
+                 title: 'Pregunta E verificación', body: '<p>Respuesta E verificación</p>',
+                 related_1_id: 1, related_2_id: 2, related_3_id: 3 }])
