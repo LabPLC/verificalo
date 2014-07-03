dcef6e06b232126f7ec24ea53c6c43dfc2e74569
Manuel Rabade <manuel@rabade.net>
2014-07-02 20:57:12 -0500

    notificaciones por correo electronico, close #192

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -6,9 +6,22 @@ class Notifier < ActionMailer::Base
   default from: ENV['VERIFICALO_SMTP_FROM'] || 'default@example.com'
 
   def confirm (user)
+    subject = 'Confirma los recordatorios de tu auto '
+    subject += user.plate
     @user = user
-    subject = "Confirma los recordatorios de tu auto "
-    subject += @user.plate
-    mail(to: @user.email.address, subject: subject)
+    mail(to: user.email.address, subject: subject)
   end  
+  
+  def weekday (user, vehicle) 
+    return unless (user.verificacion && (vehicle.verificacion_expired? ||
+                                         vehicle.verificacion_period?)) ||
+      (user.no_circula_weekday && (vehicle.no_circula_dos? ||
+                                   vehicle.no_circula_expired?)) ||
+      (user.adeudos && vehicle.adeudos?)
+    @user = user
+    @vehicle = vehicle
+    subject = 'Recordatorios de tu auto ' + user.plate
+    mail(to: user.email.address, subject: subject)
+  end
+
 end
---
+++ lib/tasks/verificalo.rake
@@ -0,0 +1,34 @@
+require 'vehicle_cdmx'
+
+namespace :verificalo do
+  
+  weekday_plates = { 
+    #'0' => '[0-9]{3}[A-Z]{3}',
+    '0' => '[0-9]{2}(5|6)[A-Z]{3}',
+    '1' => '[0-9]{2}(7|8)[A-Z]{3}',
+    '2' => '[0-9]{2}(3|4)[A-Z]{3}',
+    '3' => '[0-9]{2}(1|2)[A-Z]{3}',
+    '4' => '[0-9]{2}(9|0)[A-Z]{3}'
+  }
+
+  namespace :emails do
+    
+    desc "Send email notifications *before* weekdays (sunday to thursday)"
+    task weekday: :environment do
+      users = User.are_active.joins(:email)
+      users.where('users.plate similar to ?', weekday_plates[Date.today.wday.to_s]).each do |user|
+        3.times do
+          vehicle = VehicleCDMX.new({ plate: user.plate })
+          if vehicle.error
+            puts '! ' + user.plate + ': ' + vehicle.error
+          else
+            puts '- ' + user.plate
+            Notifier.weekday(user, vehicle).deliver
+            break
+          end
+        end
+      end
+    end
+
+  end
+end
