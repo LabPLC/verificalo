8c81cfb228fdb53352df6ed620315e4781d9875e
Manuel Rabade <manuel@rabade.net>
2014-07-06 18:00:54 -0500

    configuracion whenever

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,1 +1,11 @@
+set :environment, 'production'
 set :output, 'log/cron.log'
+job_type :rake, 'cd :path && :environment_variable=:environment bundle exec rake :task :output'
+
+every [ :sunday, :monday, :tuesday, :wednesday, :thursday ], at: '7am' do
+  rake 'verificalo:emails:weekday'
+end
+
+every :friday, at: '7am' do
+  rake 'verificalo:emails:weekend'
+end
